19435de32876589e918198a868e2559b0fa6cf21
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-16 18:21:21 +0000

    solution

--- index.js
+++ index.js
@@ -23,5 +23,41 @@ s1 and s2 consist of lowercase English letters.
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-  
+  //algo club pair programming 128 ms runtime
+    if(s1.length > s2.length) return false
+    
+    let s1Array = new Array(26).fill(0)
+    let slidingWindowArray = new Array(26).fill(0)
+    
+    for (let char of s1) {
+        // console.log(char.charCodeAt() - 97)
+        let idx = char.charCodeAt() - 97
+        s1Array[idx]++
+    }
+    
+    //do the window movement
+    for(let i = 0; i < s1.length; i++) {
+        let idx = s2[i].charCodeAt() - 97
+        slidingWindowArray[idx]++
+    
+    }
+    let start = 0, end = s1.length -1
+
+    while(end < s2.length) {
+        if(s1Array.join("") === slidingWindowArray.join("")) return true
+    
+        end++
+        if(end === s2.length) break
+        
+        let oldStart = s2[start].charCodeAt() - 97
+        let newEnd = s2[end].charCodeAt() - 97
+        
+        --slidingWindowArray[oldStart]
+        ++slidingWindowArray[newEnd]
+      
+        start++   
+
+    }
+    return false
+
 }
